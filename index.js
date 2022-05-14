cf6d4757041f1b31ae74867cbded778e4c184cea
Ulrike Werner <ulrike_werner@web.de>
2022-05-14 19:13:19 +0200

    refactoring the JS of the navigation

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { questionCards } from './assets/javaScript/card.js';
 import { navButton } from './assets/javaScript/navButton.js';
 
-const nav = document.querySelector('nav');
-const navButtons = nav.querySelectorAll('button');
+const navButtons = document.querySelector('nav').querySelectorAll('button');
 navButtons.forEach(navButton);
 
 const cards = document.querySelectorAll('.card');
