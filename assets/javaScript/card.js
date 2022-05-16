8f4c79e3dbcf5b600b43689cccc835fe84273321
Ulrike Werner <ulrike_werner@web.de>
2022-05-16 23:46:32 +0200

    create dynamic cards

--- assets/javaScript/card.js
+++ assets/javaScript/card.js
@@ -1,7 +1,7 @@
 import { bookmarkToggle } from './bookmark.js';
 import { buttonShowText } from './cardButton.js';
 
-function questionCards(card) {
+export function questionCards(card) {
   const bookmark = card.querySelector('.bookmark');
   bookmarkToggle(bookmark);
 
@@ -9,4 +9,55 @@ function questionCards(card) {
   buttonShowText(card, button);
 }
 
-export { questionCards };
+export function createCard(question) {
+  const card = document.createElement('section');
+  card.className = 'card';
+
+  const bookmark = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'svg'
+  );
+  bookmark.classList.add('bookmark');
+  if (question.isMarked) {
+    bookmark.classList.add('marked');
+  }
+  bookmark.setAttribute('viewBox', '0 0 24 24');
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  bookmark.appendChild(path);
+  card.appendChild(bookmark);
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.innerHTML = 'Question';
+  card.appendChild(cardTitle);
+
+  const cardQuestion = document.createElement('p');
+  cardQuestion.innerText = question.question;
+  card.appendChild(cardQuestion);
+
+  const buttonHolder = document.createElement('div');
+  buttonHolder.className = 'flex-row justify-content-center';
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.className = 'button';
+  button.innerText = 'Show Answer';
+  buttonHolder.appendChild(button);
+  card.appendChild(buttonHolder);
+
+  const cardAnswer = document.createElement('p');
+  cardAnswer.className = 'answer __hidden';
+  cardAnswer.innerText = question.answer;
+  card.appendChild(cardAnswer);
+
+  const tagHolder = document.createElement('div');
+  tagHolder.className = 'flex-row column-gap-0_8';
+  question.tags.forEach(tag => {
+    const tagButton = document.createElement('button');
+    tagButton.type = 'button';
+    tagButton.className = 'tag';
+    tagButton.innerText = tag;
+    tagHolder.appendChild(tagButton);
+  });
+  card.appendChild(tagHolder);
+
+  return card;
+}
---
+++ assets/javaScript/questions.js
@@ -0,0 +1,20 @@
+export const questions = [
+  {
+    question: 'question 1',
+    answer: 'answer 1',
+    tags: ['tag1'],
+    isMarked: false,
+  },
+  {
+    question: 'question 2',
+    answer: 'answer 2',
+    tags: ['lorem', 'tag 2'],
+    isMarked: false,
+  },
+  {
+    question: 'question 3',
+    answer: 'answer 3',
+    tags: ['lorem', 'tag 1', 'tag 2'],
+    isMarked: true,
+  },
+];
